847ae613d1c7d69d54ccdbfa979203932a6351d0
Oleshka24 <i.oleshka24@gmail.com>
2019-06-17 14:28:29 +0300

    Mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,69 +14,107 @@ function mute(btn,elem){
  mute($('banner__button'), 'banner__bg')
 
 // Отзывы - Карусель
+go();
+window.addEventListener('resize', go);
+function go() {	
+	if($(window).width() > 768) {
+		$('.reviews__list').trigger('destroy.owl.carousel');
+		$('.reviews__item').removeClass('reviews__item--active');
 
-function Carousel(block, element) {
-	const 	reviewCurrent = block + '__' + element + '--active',
-			reviewPrev = block + '__' + element + '--left',
-			reviewNext = block + '__' + element + '--right',
-			reviewItem = $('.' + block + '__' + element),
-
-			reviewDot = block + '__' + 'dot',
-			reviewDotCurrent = block + '__' + 'dot' + '--active';
-
-	reviewItem.addClass(reviewNext);
-
-	$(reviewItem[0]).addClass(reviewPrev);
-	$(reviewItem[0]).removeClass(reviewNext);
-
-	$(reviewItem[1]).addClass(reviewCurrent);
-	$(reviewItem[1]).removeClass(reviewNext);
-
-	$($('.' + reviewDot)[1]).addClass(reviewDotCurrent);
-
-	$('.' + reviewDot).click(function() {
-		$('.' + reviewDot).removeClass(reviewDotCurrent);
-		$(this).addClass(reviewDotCurrent);
-
-		$(reviewItem[$(this).index()]).click();
-	});
-
-	reviewItem.click(function() {
-
-		if ($(this).hasClass(reviewPrev)) {
-
-			$('.' + reviewNext).addClass(reviewPrev);
-			$('.' + reviewNext).removeClass(reviewNext);
-
-			$('.' + reviewCurrent).addClass(reviewNext);
-			$('.' + reviewCurrent).removeClass(reviewCurrent);
-
-			$(this).addClass(reviewCurrent);
-			$(this).removeClass(reviewPrev);
-
-		} else if ($(this).hasClass(reviewNext)) {
-
-			$('.' + reviewPrev).addClass(reviewNext);
-			$('.' + reviewPrev).removeClass(reviewPrev);
-
-			$('.' + reviewCurrent).addClass(reviewPrev);
-			$('.' + reviewCurrent).removeClass(reviewCurrent);
-			
-			$(this).addClass(reviewCurrent);
-			$(this).removeClass(reviewNext);
-
+		if($('.reviews__nav').length === 0) {
+			$('.reviews__list').after(`
+				<menu class="reviews__nav">
+					<button class="reviews__dot"></button>
+					<button class="reviews__dot"></button>
+					<button class="reviews__dot"></button>
+				</menu>
+			`)
 		}
 
-		let zIndex = $(this).css('z-index') - 1;
-		$(this).css('z-index', zIndex);
+		function Carousel(block, element) {
+			const 	reviewCurrent = block + '__' + element + '--active',
+					reviewPrev = block + '__' + element + '--left',
+					reviewNext = block + '__' + element + '--right',
+					reviewItem = $('.' + block + '__' + element),
 
-		if($(this).index() < 3) {
-			$($('.' + reviewDot)[$(this).index()]).click();
-		}
-	});
-};
+					reviewDot = block + '__' + 'dot',
+					reviewDotCurrent = block + '__' + 'dot' + '--active';
 
-Carousel('reviews', 'item');
+			reviewItem.addClass(reviewNext);
+
+			$(reviewItem[0]).addClass(reviewPrev);
+			$(reviewItem[0]).removeClass(reviewNext);
+
+			$(reviewItem[1]).addClass(reviewCurrent);
+			$(reviewItem[1]).removeClass(reviewNext);
+
+			$($('.' + reviewDot)[1]).addClass(reviewDotCurrent);
+
+			$('.' + reviewDot).click(function() {
+				$('.' + reviewDot).removeClass(reviewDotCurrent);
+				$(this).addClass(reviewDotCurrent);
+
+				$(reviewItem[$(this).index()]).click();
+			});
+
+			reviewItem.click(function() {
+
+				if ($(this).hasClass(reviewPrev)) {
+
+					$('.' + reviewNext).addClass(reviewPrev);
+					$('.' + reviewNext).removeClass(reviewNext);
+
+					$('.' + reviewCurrent).addClass(reviewNext);
+					$('.' + reviewCurrent).removeClass(reviewCurrent);
+
+					$(this).addClass(reviewCurrent);
+					$(this).removeClass(reviewPrev);
+
+				} else if ($(this).hasClass(reviewNext)) {
+
+					$('.' + reviewPrev).addClass(reviewNext);
+					$('.' + reviewPrev).removeClass(reviewPrev);
+
+					$('.' + reviewCurrent).addClass(reviewPrev);
+					$('.' + reviewCurrent).removeClass(reviewCurrent);
+					
+					$(this).addClass(reviewCurrent);
+					$(this).removeClass(reviewNext);
+
+				}
+
+				let zIndex = $(this).css('z-index') - 1;
+				$(this).css('z-index', zIndex);
+
+				if($(this).index() < 3) {
+					$($('.' + reviewDot)[$(this).index()]).click();
+				}
+			});
+		};
+
+		Carousel('reviews', 'item');
+	} else {
+		$('.reviews__item').addClass('reviews__item--active');
+		$('.reviews__item').removeClass('reviews__item--left');
+		$('.reviews__item').removeClass('reviews__item--right');
+		$('.reviews__dot').removeClass('reviews__dot--active');
+
+		$('.reviews__list').owlCarousel({
+			items: 1,
+			loop: true,
+			nav: false,
+			dots: true,
+			dotClass: "reviews__dot",
+			dotsContainer: $(".reviews__nav"),
+		});
+
+		// Owl Fix
+
+		$('.owl-stage-outer').css('overflow', 'hidden');
+		$('.owl-stage').css('display', 'flex');
+		$('.disabled').remove();
+	}
+}
 
 // Отзывы - Кнопка
 
